63c9cc9760dc113bfab4617e097b3b27d7257374
tony <tony.s@mehery.com>
2023-09-12 03:13:07 +0530

    added command

--- nodemon-ngrok.js
+++ nodemon-ngrok.js
@@ -1,6 +1,7 @@
 const nodemon = require("nodemon");
 const {setup} = require("./app/service/ngrok");
 setup().then(function({ url }){
+    console.log(" process.argv", process.argv[1], process.argv[2])
     nodemon({
       script: process.argv[2],
       exec: `NGROK_URL=${url} node `,
