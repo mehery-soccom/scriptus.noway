276c6ab970892ecdc53abf7b1e015628a4e2b065
tony <tony.s@mehery.com>
2023-09-23 23:13:21 +0530

    package update

--- app/service/ngrok.js
+++ app/service/ngrok.js
@@ -2,17 +2,51 @@
 const config = require('@bootloader/config');
 const ngrok = require("@ngrok/ngrok");
 const port = process.env.PORT || config.get("server.port");
+var prompt = require('prompt');
+const fs = require('fs')
 
 const NGROK_URL = config.getIfPresent("NGROK_URL");
-console.log("ngrok.auth.token",config.get("ngrok.auth.token"));
+const NGROK_TOKEN = config.get("ngrok.auth.token");
+console.log("ngrok.auth.token",NGROK_TOKEN);
 console.log("NGROK_URL",NGROK_URL);
 console.log("*************************");
+
+function ngrokProperties({token,domain}){
+return `
+####### NGROK ##############                                 
+ngrok.domain=${token}
+ngrok.auth.token=${domain}
+`
+}
 
 let flags = {
     setup : null,
 }
 
-module.exports = {
+let ngrokspace = {
+    async start_ngrok({token,domain}) {
+        await ngrok.authtoken(token)
+        // await ngrok.connect({
+        //   authtoken : config.get("ngrok.auth.token"),
+        //   //domain : config.get("ngrok.domain")
+        // });
+        const session = await new ngrok.NgrokSessionBuilder()
+        .authtoken(token)
+        //.authtokenFromEnv()
+        .connect();
+        const ssssn =  session.httpEndpoint();
+        if(domain){
+            ssssn.domain(domain)
+        }
+        const tunnel = await ssssn.listen();
+        //.allowCidr("0.0.0.0/0")
+        //const socket = await ngrok.listen(app, tunnel);
+        console.log(`Ingress established at: ${tunnel.url()}`);
+        //console.log(`Express listening on: ${socket.address()}`);
+        tunnel.forward("localhost:"+port);
+        config.store("ngrok").url = NGROK_URL;
+        return {tunnel};
+    },    
     async setup() {
         console.log("NGROK SETUP", NGROK_URL)
         if(!flags.setup){
@@ -24,30 +58,40 @@ module.exports = {
                     })
                 });
             } else {
-                flags.setup = new Promise(async function(resolve, reject){
+                let THAT = this;
+                flags.setup = new Promise(async (resolve, reject)=>{
                     try {
-                        await ngrok.authtoken(config.get("ngrok.auth.token"))
-                        // await ngrok.connect({
-                        //   authtoken : config.get("ngrok.auth.token"),
-                        //   //domain : config.get("ngrok.domain")
-                        // });
-                        const session = await new ngrok.NgrokSessionBuilder()
-                        .authtoken(config.get("ngrok.auth.token"))
-                        //.authtokenFromEnv()
-                        .connect();
-                        const tunnel = await session.httpEndpoint()
-                        //.allowCidr("0.0.0.0/0")
-                        .domain(config.get("ngrok.domain"))
-                        .listen();
-                        //const socket = await ngrok.listen(app, tunnel);
-                        console.log(`Ingress established at: ${tunnel.url()}`);
-                        //console.log(`Express listening on: ${socket.address()}`);
-                        tunnel.forward("localhost:"+port);
-                        config.store("ngrok").url = NGROK_URL;
-                        resolve({
-                            url : tunnel.url()
-                        });
+                        if(NGROK_TOKEN){
+                            let { tunnel } = await THAT.start_ngrok({
+                                domain : config.getIfPresent("ngrok.domain"),
+                                token : config.get("ngrok.auth.token")
+                            });
+                            resolve({
+                                url : tunnel.url()
+                            });
+                        } else {
+                            prompt.start();
+                            prompt.get(['NGROK_AUTH_TOKEN', 'NGROK_DOMAIN'], function (err, result){
+                                if (!fs.existsSync("./config/local.properties")) {
+                                    fs.writeFileSync("./config/local.properties",ngrokProperties({
+                                        token : result.NGROK_AUTH_TOKEN,
+                                        domain : result.NGROK_DOMAIN
+                                    }), function (err) {
+                                        if (err) throw err;
+                                    });
+                                }
+                                let {tunnel} = THAT.start_ngrok({
+                                    token : result.NGROK_AUTH_TOKEN,
+                                    domain : result.NGROK_DOMAIN
+                                }).then(()=>{
+                                    resolve({
+                                        url : tunnel.url()
+                                    });
+                                });
+                            });
+                        }
                     } catch(e){
+                        console.log("Failed====",e)
                         reject(e);
                     }
                 });
@@ -56,3 +100,9 @@ module.exports = {
         return flags.setup;
       }
   }
+
+  module.exports = {
+        async setup(){
+            return await ngrokspace.setup();
+        }
+  };
