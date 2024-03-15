5e3104767176987e3c86ae7ef1d439ce2ab2626b
lalit.tanwar <lalit.tanwar07@gmail.com>
2024-03-15 19:32:49 +0530

    NGROK URL update

--- start.js
+++ start.js
@@ -9,6 +9,8 @@ var server = http.createServer(app);
 
 //Lets start our server
 server.listen(port, function(){
+    console.log("NGROK_URL:3",config.getIfPresent("NGROK_URL"))
+    console.log("ngrok.url:3",config.store("ngrok").url)
     //Callback triggered when server is successfully listening. Hurray!
     console.log("Server listening on: http://localhost:%s", port);
     noway.emit('noway.started', null)
